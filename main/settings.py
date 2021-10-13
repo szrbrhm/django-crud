f29eb56265cf0986f6a6d84fe90d6cab2c3da0ed
aaron <aaron@nioyatech.com>
2021-10-13 14:26:25 +0300

    feat: add media

--- main/settings.py
+++ main/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#Static Files Settings
 STATIC_URL = '/static/'
+STATIC_ROOT = [BASE_DIR,   'static-files']
+
+#Uploaded_files Settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR, 'media']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
