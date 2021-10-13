fc21734e3c0a493a47c74823ad0b4f503ca34dbf
aaron-clarusway <67314728+aaron-clarusway@users.noreply.github.com>
2021-10-13 15:08:13 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = [BASE_DIR,   'static-files']
 
 #Uploaded_files Settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [BASE_DIR, 'media']
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
