8e34d41446b05ee7aeeca3ef838cfcbe6cddd98f
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-18 19:40:38 +0200

    add student table

--- fscohort/models.py
+++ fscohort/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    
+    GENDER = (
+        ("1","Female")
+        ("2","Male")
+        ("3","Other")
+        ("4","Prefer Not Say")
+    )    
+    gender = models.CharField(max_length=40, choices=GENDER)
+    number = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="student/", default="avatar.png")
